e228b1d4a7d17e6e87a4fec3e108cc0fba6fa356
villancikos <villancikos@gmail.com>
2014-09-17 17:35:43 -0500

    fixed URLS and others

--- django_ecommerce/django_ecommerce_project/django_ecommerce_project/urls.py
+++ django_ecommerce/django_ecommerce_project/django_ecommerce_project/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from payments import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,4 +16,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.index', name='home'),
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^contact/', 'contact.views.contact', name='contact'),
+    url(r'^sign_in$', views.sign_in, name='sign_in'),
+    url(r'^sign_out$', views.sign_out, name="sign_out"),
+    url(r'^register$', views.register, name="register"),
+    url(r'^edit$', views.edit, name='edit'),
 )
